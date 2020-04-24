bcf6c34cc757e6ff52a1e0527c48497a0fcc1c56
xXHachimanXx <globaldep90@gmail.com>
2020-04-24 19:36:02 -0300

    Adicionando mensagem de vencedor.

--- src/index.js
+++ src/index.js
@@ -34,13 +34,22 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // slice cria uma cópia dos arrays
-    squares[i] = this.state.xIsNext? 'X' : 'O';
+    if(calculateWinner(squares) || squares[i]){ return; }
 
+    squares[i] = this.state.xIsNext? 'X' : 'O';
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext }); // Setar novo valor para vetor de quadrados
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    
+    if(winner) {
+      status = 'Winner: ' + winner;
+    }
+    else {
+      status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -65,7 +74,8 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+class Game extends React.Component {  
+
   render() {
     return (
       <div className="game">
@@ -89,3 +99,23 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
