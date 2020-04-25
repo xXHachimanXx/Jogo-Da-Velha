e041bb80aea232136cc16a49665ad6ca2b27d734
xXHachimanXx <globaldep90@gmail.com>
2020-04-25 11:31:41 -0300

    Outra tentativa de deploy.

---
+++ precache-manifest.525612e1c5dccec33b07bfaa3e1de0d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644b7316ef056a914f481959e654fa5a",
+    "url": "/Jogo-Da-Velha/index.html"
+  },
+  {
+    "revision": "a44eac0fce907bd315a5",
+    "url": "/Jogo-Da-Velha/static/css/main.3c1c4497.chunk.css"
+  },
+  {
+    "revision": "f02782475be02e58c3ca",
+    "url": "/Jogo-Da-Velha/static/js/2.a1ed38e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jogo-Da-Velha/static/js/2.a1ed38e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44eac0fce907bd315a5",
+    "url": "/Jogo-Da-Velha/static/js/main.158e76e0.chunk.js"
+  },
+  {
+    "revision": "16055c27893213c2c5ee",
+    "url": "/Jogo-Da-Velha/static/js/runtime-main.d6444d3d.js"
+  }
+]);
